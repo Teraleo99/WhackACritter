93708e4a8dfad07de0172dd80c72b1e18402798f
Teraleo99 <43067374+Teraleo99@users.noreply.github.com>
2018-09-21 15:43:40 +0100

    Timer implemented

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    //Display visual timer
+    public TextMesh displayText;
+
+    //Starting time for timer
+    public float timerDuration;
+
+    //Current seconds remaining on the timer
+    private float timeRemaining = 0; 
+
+	// Use this for initialization
+	void Start () {
+        StartTimer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Only process the timer if it's running
+
+		if (IsTimerRunning())
+            {
+
+            //Timer is running, so process
+            //Updated the time remaining per frame
+            timeRemaining = timeRemaining - Time.deltaTime;
+
+            //Check if we have NOW run out of time
+            if (timeRemaining <= 0)
+            {
+                StopTimer();
+            }
+            //Update the visual display
+            displayText.text = Mathf.CeilToInt(timeRemaining).ToString();
+        }
+	}
+    //Starts timer counting
+    public void StartTimer()
+    {
+        timeRemaining = timerDuration;
+        displayText.text = Mathf.CeilToInt(timeRemaining).ToString();
+    }
+    //Stop the timer counting
+    public void StopTimer()
+    {
+        timeRemaining = 0;
+        displayText.text = Mathf.CeilToInt(timeRemaining).ToString();
+    }
+    //Is the timer currently running
+    public bool IsTimerRunning()
+    {
+        if (timeRemaining != 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
