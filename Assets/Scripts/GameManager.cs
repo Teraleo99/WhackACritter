a0a07a806c33320c8363554dbfdac547bac9efda
Teraleo99 <43067374+Teraleo99@users.noreply.github.com>
2018-09-21 15:03:41 +0100

    Critters finished

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public List<GameObject> critterPrefabs;
+    public float critterSpawnFrequency = 1.0f;
+    private float lastCritterSpawn = 0;
+    public Score scoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Check if it's time to spawn critter
+        float nextSpawnTime = lastCritterSpawn + critterSpawnFrequency;
+
+        if (Time.time >= nextSpawnTime)
+        {
+            //It is time!
+            //Choose a random critter to spawn
+            int critterIndex = Random.Range(0, critterPrefabs.Count);
+            GameObject prefabToSpawn = critterPrefabs[critterIndex];
+
+            //Spawn the critter
+            GameObject spawnedCritter = Instantiate(prefabToSpawn);
+
+            //Get access to critter script
+            Critter critterScript = spawnedCritter.GetComponent<Critter>();
+
+            //Tell critter script score object
+            critterScript.scoreDisplay = scoreDisplay;
+
+            //Update most recent spawn time to now
+            lastCritterSpawn = Time.time;
+        }
+	}
+}
