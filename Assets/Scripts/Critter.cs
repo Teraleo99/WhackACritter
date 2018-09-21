bf5cc096219c805969ed96ee3331146e5e216cdd
Teraleo99 <43067374+Teraleo99@users.noreply.github.com>
2018-09-21 16:51:28 +0100

    Moving critters

--- Assets/Scripts/Critter.cs
+++ Assets/Scripts/Critter.cs
@@ -12,15 +12,28 @@ public class Critter : MonoBehaviour {
 
     public int pointValue = 1; //How much is this critter worth?
 
+    private Vector2 direction; //What direction
+
     // Use this for initialization
     void Start () {
         transform.position = new Vector3(Random.Range(lowerRange.x, upperRange.x),
             Random.Range(lowerRange.y, upperRange.y), 
             0);
-	}
+
+        //Pick random direction for critter
+        direction.x = Random.Range(-1.0f, 1.0f);
+        direction.y = Random.Range(-1.0f, 1.0f);
+        direction = direction.normalized;
+
+        //Rotate our critter in this direction 
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+    }
 
 	// Update is called once per frame
 	void Update () {
+
+        transform.Translate(Vector2.up * Time.deltaTime);
+
 		if (timer.IsTimerRunning() == false)
         {
             Destroy(gameObject);
