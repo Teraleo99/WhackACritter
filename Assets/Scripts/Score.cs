6b640dda281ea1d6b000818bb0ca03a588852dad
Teraleo99 <43067374+Teraleo99@users.noreply.github.com>
2018-09-21 14:16:51 +0100

    Basic score complete

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    //Public variables visible in unity
+    public TextMesh displayText;
+
+    //Private variables can;t be touched by other scripts
+    private int currentValue = 0; 
+	
+    //Update both the data value and the visual display
+    public void ChangeValue(int _toChange)
+    {
+        currentValue = currentValue + _toChange;
+        displayText.text = currentValue.ToString();
+    }
+}
